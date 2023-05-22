115e144c96bb06aa1e811ee4d53d79b5e49e78f2
Estelle Dose <estelle.dose@laplateforme.io>
2023-05-22 11:06:59 +0200

    php

---
+++ jour01/job01/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Job 01</title>
+</head>
+<body>
+    <?php
+        $str = "LaPlateforme";
+        echo $str;
+
+        $str2 = "Vive";
+        $str3 = "!";
+
+        echo $str2 . " " . $str . " " . $str3;
+
+        $val = 6;
+        echo "<br>" . $val;
+
+        $val += 4;
+        echo " " . $val;
+
+        $myBool = true;
+        var_dump($myBool);
+
+        $myBool = false;
+        var_dump($myBool);
+    ?>
+</body>
+</html>
