3b58b7a524820d21b59646cbfa2a2ec9a6651b7d
Estelle Dose <estelle.dose@laplateforme.io>
2023-05-23 10:18:20 +0200

    php

---
+++ jour02/job02/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>job02</title>
+</head>
+<body>
+    <?php
+        for ($i = 0; $i <= 1337; $i++) {
+            if ($i == 26 || $i == 37 || $i == 88 || $i == 1111 || $i == 3233) {
+                continue;
+            }
+            
+            echo $i . "<br />";
+        }
+    ?>
+</body>
+</html>
---
+++ jour02/job03/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>job03</title>
+</head>
+<body>
+    <?php
+        for ($i = 0; $i <= 100; $i++) {
+            if ($i >= 0 && $i <= 20) {
+                echo "<i>" . $i . "</i><br />";
+            } elseif ($i >= 25 && $i <= 41) {
+                echo "<u>" . $i . "</u><br />";
+            } elseif ($i >= 43 && $i <= 50) {
+                echo "<u>" . $i . "</u><br />";
+            } elseif ($i == 42) {
+                echo "La Plateforme_<br />";
+            } else {
+                echo $i . "<br />";
+            }
+        }
+    ?>
+</body>
+</html>
